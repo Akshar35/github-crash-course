e43422f963793e96dd1de8c988ec8e7eb1bebed1
[Akshar jha] <[aksharjha35@gmail.com]>
2026-01-17 16:51:28 +0530

    first.js file added

--- utils/script.js
+++ utils/script.js
@@ -1,3 +1,21 @@
 function sayHello(){
     console.log("Hello World");
 }
+sayHello();
+
+
+function printName(name1){
+    console.log("My name is " + name1);
+}
+printName(Akshar);
+
+
+function isAbove80( number){
+    return number > 80;
+}
+isAbove80(85)
+
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+addNumbers(5, 10);
