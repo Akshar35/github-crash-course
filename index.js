b7264a1e2db5fba09c353e26444262b8c320d41e
[Akshar jha] <[aksharjha35@gmail.com]>
2026-01-17 09:39:34 +0530

    printName function Added in script.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const age = 21
+
+function printTwoNumbers(num1, num2) {
+  console.log(num1);
+  console.log(num2);
+}
+
+printTwoNumbers(5, 10);
+
+const name = "lakshyyy";
+const dob = "01-01-2005"
+
+function sayAge(){
+    console.log('My age is ' + age);
+}
