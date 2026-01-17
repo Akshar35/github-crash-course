6073eea4ce50ecef9f6092813ec0548400f4b011
[Akshar jha] <[aksharjha35@gmail.com]>
2026-01-17 21:15:58 +0530

    details about added feature

--- first.js
+++ first.js
@@ -2,5 +2,6 @@ console.log("First JavaScript File");
 
 function newFeatureAdded(){
     console.log("A new feature has been added!");
+    console.log("This is advance feature!");
 }
 newFeatureAdded();
